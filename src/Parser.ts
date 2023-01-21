427311f184ba29faa2eb9f5899f31cadeb900f84
Erdem <zahrakon06@gmail.com>
2023-01-21 17:10:32 +0300

    update

---
+++ src/Parser.ts
@@ -0,0 +1,57 @@
+export type ParsedXP = {
+  level: number,
+  nextLevel: number,
+  neededXp: number,
+  currentXp: number,
+  currentLevelXp: number,
+  nextLevelXp: number,
+  currentRelativeXp: number,
+  nextRelativeXp: number,
+}
+
+export class Parser {
+    /** @private */
+    exponant: number;
+    /** @private */
+    base: number;
+    /** @private */
+    multiplier: number;
+    constructor(base: number = 0, multiplier: number = 100, exponant: number = 2) {
+        this.exponant = exponant;
+        this.base = base;
+        this.multiplier = multiplier;
+    }
+
+    // parse xp to level
+    parseXpToLevel(xp: number): number {
+        return Math.floor(Math.pow((xp - this.base) / this.multiplier, 1 / this.exponant));
+    }
+
+    // parse level to xp
+    parseLevelToXp(level: number): number {
+        return Math.floor(this.base + this.multiplier * Math.pow(level, this.exponant));
+    }
+
+    // parse xp to next level and current level and needed xp and current xp and current level xp and next level xp
+    parseXp(xp: number): ParsedXP {
+        const level = this.parseXpToLevel(xp);
+        const nextLevel = level + 1;
+        const neededXp = this.parseLevelToXp(nextLevel) - xp;
+        const currentXp = xp;
+        const currentLevelXp = this.parseLevelToXp(level);
+        const nextLevelXp = this.parseLevelToXp(nextLevel + 1);
+        const currentRelativeXp = currentXp - currentLevelXp;
+        const nextRelativeXp = nextLevelXp - currentLevelXp;
+        
+        return {
+            level,
+            nextLevel,
+            neededXp,
+            currentXp,
+            currentLevelXp,
+            nextLevelXp,
+            currentRelativeXp,
+            nextRelativeXp,
+        };
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// export default from "./Parser.ts"
+export { Parser, ParsedXP } from "./Parser";
